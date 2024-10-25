e44e99d6cd358ba22cec2bf3d332c8ec7ad63f66
Anton Pampukha <antonpampuha4@gmail.com>
2024-10-25 20:33:26 +0300

    carry out all requirements to pass lab 4 +complete and Eulerian graphs check

--- Program.cs
+++ Program.cs
@@ -5,99 +5,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        string path = "matrix3.txt";
+        string path = "matrix1dir.txt";
         int[,] incidenceMatrix = ReadMatrixFromFile(path);
         if (!IsCorrectMatrix(incidenceMatrix))
         {
             Console.WriteLine("Not correct matrix");
             return;
         }
-        Console.WriteLine([]);
+        //ShowMatrix(incidenceMatrix, "Incidence matrix");
 
-        int[,] newMat = ConvertToMatrix(incidenceMatrix);
-        int k =  newMat.GetLength(0);
-        for (int i = 0; i < k; i++)
+        #region Choice type of graph  
+        bool isDirGraph = false;
+        Console.Write("Your matrix is dir? (y/n): ");
+        string? buff = Console.ReadLine();
+        if (buff == "y")
+            isDirGraph = true;
+        else if (buff == "n")
+            isDirGraph = false;
+        else
         {
-            for (int j = 0; j < k; j++)
-            {
-                Console.Write($"{newMat[i, j]}  ");
-            }
-            Console.WriteLine();
+            Console.WriteLine("Not correct input");
+            return;
         }
-
-        int isComplete = 0;
-        bool isEulerian = false;
-
-        #region switch
-        //if (ChoiceUndirectedOrDirectedGraph())
-        //{
-        //    int type = ChoiceTypeOfGraph();
-        //    switch (type)
-        //    {
-        //        case 1:
-        //            isComplete = IsUndirectedSimpleGraphComplete(incidenceMatrix);
-        //            isEulerian = IsEulerianUndirectedSimpleGraph(incidenceMatrix);
-        //            break;
-        //        case 2:
-        //            //isComplete = IsUndirectedPseudographComplete(incidenceMatrix);
-        //            isComplete = 2;
-        //            isEulerian = IsEulerianUndirectedPseudograph(incidenceMatrix);
-        //            break;
-        //        case 3:
-        //            //isComplete = IsUndirectedMultigraphComplete(incidenceMatrix);
-        //            isComplete = 2;
-        //            isEulerian = IsEulerianUndirectedMultigraph(incidenceMatrix);
-        //            break;
-        //        default:
-        //            Console.WriteLine("False choice.");
-        //            return;
-        //    }
-        //}
-        //else
-        //{
-        //    int type = ChoiceTypeOfGraph();
-        //    switch (type)
-        //    {
-        //        case 1:
-        //            isComplete = IsDirectedSimpleGraphComplete(incidenceMatrix);
-        //            if (isComplete == 1)
-        //                isEulerian = true;
-        //            else
-        //                isEulerian = IsEulerianDirectedSimpleGraph(incidenceMatrix);
-        //            break;
-        //        case 2:
-
-        //            break;
-        //        default:
-        //            Console.WriteLine("Невірний вибір.");
-        //            return;
-        //    }
-        //}
         #endregion
 
-        if (isComplete == 1)
+        int[,] adjacencyMatrix;
+        try
         {
-            Console.WriteLine("Граф є повним.");
+            adjacencyMatrix = isDirGraph ?
+                ConvertIncidenceToAdjacencyMatrixDirGraph(incidenceMatrix) :
+                ConvertIncidenceToAdjacencyMatrixUndirGraph(incidenceMatrix);
         }
-        else if (isComplete == 0)
+        catch
         {
-            Console.WriteLine("Граф не є повним.");
-        }
-        else
-        {
-            Console.WriteLine("Graph can't be completed");
+            Console.WriteLine("Not matching matrix type");
+            return;
         }
 
-        if (isEulerian)
+        ShowMatrix(adjacencyMatrix, "Adjacency matrix:");
+
+        if (isDirGraph)
         {
-            Console.WriteLine("Граф є ейлеровим.");
+            if (IsCompleteDirGraph(adjacencyMatrix))
+                Console.WriteLine("Complete graph");
+            else
+                Console.WriteLine("Not complete graph");
         }
         else
         {
-            Console.WriteLine("Граф не є ейлеровим.");
+            if (IsCompleteUndirGraph(adjacencyMatrix))
+                Console.WriteLine("Complete graph");
+            else
+                Console.WriteLine("Not complete graph");
         }
+
+        if (isDirGraph)
+        {
+            if (IsEulerianDirGraph(adjacencyMatrix))
+                Console.WriteLine("Eulerian graph");
+            else
+                Console.WriteLine("Not Eulerian graph");
+        }
+        else
+        {
+            if (IsEulerianUndirGraph(adjacencyMatrix))   //easily using func "IsEulerianDirGraph"
+                Console.WriteLine("Eulerian graph");     //to avoid copy code
+            else
+                Console.WriteLine("Not Eulerian graph");
+        }
+
     }
-
 
     static int[,] ReadMatrixFromFile(string path)
     {
@@ -140,75 +117,31 @@ class Program
         }
         return true;
     }
-    static public bool ChoiceUndirectedOrDirectedGraph()
+    static public void ShowMatrix(int[,] matrix, string message = "Matrix")
     {
-        Console.WriteLine("Оберіть тип графа:");
-        Console.WriteLine("1. Undir");
-        Console.WriteLine("2. Dir");
-        int num = int.Parse(Console.ReadLine());
-        while (true)
+        Console.WriteLine(message);
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            switch (num)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                case 1:
-                    return true;
-                case 2:
-                    return false;
-                default:
-                    Console.WriteLine("Not correct input");
-                    break;
+                Console.Write($"{matrix[i, j]}  ");
             }
+            Console.WriteLine();
         }
     }
-    static public int ChoiceTypeOfGraph()
-    {
-        while (true)
-        {
-            Console.WriteLine("Оберіть тип графа:");
-            Console.WriteLine("1. Простий граф");
-            Console.WriteLine("2. Псевдограф");
-            Console.WriteLine("3. Мультиграф");
-            int choice = int.Parse(Console.ReadLine());
-            if (choice == 1 || choice == 2 || choice == 3)
-                return choice;
-            else Console.WriteLine("Not correct input");
-        }
 
-    }
-
-    static public int[,] ConvertToMatrix(int[,] matrix)
+    static public int[,] ConvertIncidenceToAdjacencyMatrixUndirGraph(int[,] matrix)
     {
         int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(0)];
-        //int length = newMatrix.GetLength(0); = 3
+
 
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            //int firstIndex = -1;
-            //int secondIndex = -1;
-            //int loop = -1;
-            //bool isUsed = false;
+
             List<int> index = new() { Capacity = 2 };
             //Search index
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                /*if (matrix[i, j] == 1 && firstIndex != 1)
-                {
-                    firstIndex = i;
-                    isUsed = true;
-                    continue;
-                }
-                if (matrix[i, j] == 1 && isUsed)
-                {
-                    secondIndex = i;
-                    isUsed = false;
-                }
-                if (matrix[i, j] == 2)
-                {
-                    loop = i;
-                }
-                Console.WriteLine("--------");
-                Console.WriteLine(firstIndex);
-                Console.WriteLine(secondIndex);*/
                 if (matrix[i, j] == 1 || matrix[i, j] == 2)
                     index.Add(i);
             }
@@ -229,230 +162,106 @@ class Program
         }
         return newMatrix;
     }
+    static public int[,] ConvertIncidenceToAdjacencyMatrixDirGraph(int[,] matrix)
+    {
+
+        int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(0)];
 
 
-    ////НЕОРІЄНТОВАНІ
-    //// Метод для перевірки, чи є простий граф повним
-    //static int IsUndirectedSimpleGraphComplete(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int edges = matrix.GetLength(1);
-    //    int expectedEdges = (vertices * (vertices - 1)) / 2;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
 
-    //    if (edges != expectedEdges)
-    //    {
-    //        return 0;
-    //    }
+            //List<int> index = new() { Capacity = 2 };
+            Dictionary<int, int> index = new();
 
-    //    return 1;
-    //}
+            //Search index
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, j] == 1) index.Add(1, i);
 
-    // //Метод для перевірки, чи є псевдограф повним
-    //static bool IsUndirectedPseudographComplete(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int edges = matrix.GetLength(1);
-    //    int expectedEdges = (vertices * (vertices - 1)) / 2 + vertices; // Враховуємо петлі до кожної вершини
+                if (matrix[i, j] == -1) index.Add(-1, i);
 
-    //    if (edges < expectedEdges)
-    //    {
-    //        return false;
-    //    }
+                if (matrix[i, j] == 2) index.Add(2, i);
+            }
 
-    //    return true;
-    //}
+            //Put index in new matrix
+            //Edges
+            if (index.Count == 2)
+            {
+                if (index.Keys.FirstOrDefault(x => x == 1) == 1)
+                    newMatrix[index[-1], index[1]]++;
+                else if (index.Keys.FirstOrDefault(x => x == -1) == -1)
+                    newMatrix[index[1], index[-1]]++;
+            }
+            //Loop
+            if (index.Count == 1)
+            {
+                newMatrix[index[2], index[2]] += 2;
+            }
 
-    ////Метод для перевірки, чи є мультиграф повним
-    //static bool IsUndirectedMultigraphComplete(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int edges = matrix.GetLength(1);
-    //    int expectedEdges = (vertices * (vertices - 1)) / 2;
-
-    //    if (edges < expectedEdges)
-    //    {
-    //        return false;
-    //    }
-
-    //    return true;
-    //}
-
-    ////Перевірка ейлеровості для простого графа
-
-    //static bool IsEulerianUndirectedSimpleGraph(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int[] degrees = new int[vertices];
-
-    //    // Обчислюємо степінь кожної вершини
-    //    for (int j = 0; j < matrix.GetLength(1); j++)
-    //    {
-    //        for (int i = 0; i < vertices; i++)
-    //        {
-    //            if (matrix[i, j] != 0)
-    //            {
-    //                degrees[i]++;
-    //            }
-    //        }
-    //    }
-
-    //    // Перевіряємо на парність степенів
-    //    foreach (var degree in degrees)
-    //    {
-    //        if (degree % 2 != 0)
-    //        {
-    //            return false; // Якщо є непарна степінь, граф не є ейлеровим
-    //        }
-    //    }
-
-    //    return true;
-    //}
-
-    //// Перевірка ейлеровості для псевдографа
-    //static bool IsEulerianUndirectedPseudograph(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int[] degrees = new int[vertices];
-
-    //    // Обчислюємо степінь кожної вершини, враховуючи петлі
-    //    for (int j = 0; j < matrix.GetLength(1); j++)
-    //    {
-    //        int loopCount = 0;
-    //        for (int i = 0; i < vertices; i++)
-    //        {
-    //            if (matrix[i, j] == 2) // Петля додає два до степеня вершини
-    //            {
-    //                degrees[i] += 2;
-    //                loopCount++;
-    //            }
-    //            else if (matrix[i, j] == 1)
-    //            {
-    //                degrees[i]++;
-    //            }
-    //        }
-
-    //        if (loopCount > 1) // Якщо є більше ніж одна петля на одному ребрі
-    //        {
-    //            return false;
-    //        }
-    //    }
-
-    //    // Перевіряємо на парність степенів
-    //    foreach (var degree in degrees)
-    //    {
-    //        if (degree % 2 != 0)
-    //        {
-    //            return false;
-    //        }
-    //    }
-
-    //    return true;
-    //}
-
-    //// Перевірка ейлеровості для мультиграфа
-    //static bool IsEulerianUndirectedMultigraph(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int[] degrees = new int[vertices];
-
-    //    // Обчислюємо степінь кожної вершини, враховуючи кратні ребра
-    //    for (int j = 0; j < matrix.GetLength(1); j++)
-    //    {
-    //        for (int i = 0; i < vertices; i++)
-    //        {
-    //            if (matrix[i, j] != 0)
-    //            {
-    //                degrees[i]++;
-    //            }
-    //        }
-    //    }
-
-    //    // Перевіряємо на парність степенів
-    //    foreach (var degree in degrees)
-    //    {
-    //        if (degree % 2 != 0)
-    //        {
-    //            return false;
-    //        }
-    //    }
-
-    //    return true;
-    //}
+        }
+        return newMatrix;
+    }
 
 
+    static public bool IsCompleteUndirGraph(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = i + 1; j < matrix.GetLength(0); j++)
+            {
+                if (matrix[i, j] == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+    static public bool IsCompleteDirGraph(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                if (matrix[i, j] == 0 && i != j)
+                    return false;
+            }
+        }
+        return true;
+    }
+    static public bool IsEulerianUndirGraph(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[i, j];
+            }
+            if (sum % 2 != 0)
+                return false;
+        }
+        return true;
+    }
+    static public bool IsEulerianDirGraph(int[,] matrix)
+    {
+        int length = matrix.GetLength(0);
+        for (int i = 0; i < length; i++)
+        {
+            int sumRow = 0;
+            int sumCol = 0;
 
-    ////ОРІЄНТОВАНІ
-    //// Метод для перевірки, чи є простий граф повним
-    //static int IsDirectedSimpleGraphComplete(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int edges = matrix.GetLength(1);
-    //    int expectedEdges = vertices * (vertices - 1);
+            for (int j = 0; j < length; j++)
+            {
+                sumRow += matrix[i, j];
+            }
 
-    //    if (edges != expectedEdges)
-    //    {
-    //        return 0;
-    //    }
-
-    //    //перевірка матриці???
-    //    //for (int j = 0; j < edges; j++)
-    //    //{
-    //    //    int toVertex = 0;
-    //    //    int fromVertex = 0;
-    //    //    for (int i = 0; i < vertices; i++)
-    //    //    {
-    //    //        if (matrix[i, j] == 1) toVertex++;
-    //    //        if (matrix[i, j] == -1) fromVertex++;
-    //    //    }
-
-    //    //    if (toVertex != 1 || fromVertex != 1)
-    //    //    {
-    //    //        return false;
-    //    //    }
-    //    //}
-
-    //    return 1;
-    //}
-
-    ////// Метод для перевірки, чи є мультиграф повним
-    ////static bool IsDirectedMultiraphComplete(int[,] matrix) { return false; }
-
-    ////// Метод для перевірки, чи є псевдограф повним
-    ////static bool IsDirectedPseudographComplete(int[,] matrix) { return false; }
-
-    //// Перевірка ейлеровості для простого графа
-
-    //static bool IsEulerianDirectedSimpleGraph(int[,] matrix)
-    //{
-    //    int vertices = matrix.GetLength(0);
-    //    int[] inDegrees = new int[vertices];
-    //    int[] outDegrees = new int[vertices];
-
-    //    // Обчислюємо вхідні і вихідні степені для кожної вершини
-    //    for (int j = 0; j < matrix.GetLength(1); j++)
-    //    {
-    //        for (int i = 0; i < vertices; i++)
-    //        {
-    //            if (matrix[i, j] == 1) outDegrees[i]++;
-    //            if (matrix[i, j] == -1) inDegrees[i]++;
-    //        }
-    //    }
-
-    //    // Перевіряємо чи для кожної вершини вхідні степені рівні вихідним
-    //    for (int i = 0; i < vertices; i++)
-    //    {
-    //        if (inDegrees[i] != outDegrees[i])
-    //        {
-    //            return false;
-    //        }
-    //    }
-
-    //    return true;
-    //}
-
-    ////Перевірка ейлеровості для мультиграфа
-    //static bool IsEulerianDirectedMultigraph(int[,] matrix) { return false; }
-
-    ////Перевірка ейлеровості для простого графа
-    //static bool IsEulerianDirectedPseudograph(int[,] matrix) { return false; }
+            for (int k = 0; k < length; k++)
+            {
+                sumCol += matrix[k, i];
+            }
+            if (sumCol != sumRow)
+                return false;
+        }
+        return true;
+    }
 }
